4757f2fa5f94a4a4a74591075079b3a0bb09159c
fabio <fabiofelix@hotmail.com>
2022-02-15 16:42:36 -0300

    altrando tempo de validade do token para 300s

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: "120s"},
+      signOptions: { expiresIn: "300s"},
     })
 
   ],
