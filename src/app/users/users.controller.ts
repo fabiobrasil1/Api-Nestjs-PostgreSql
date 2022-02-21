5632d5f098cc40bfb194ee1c703ac4cbfe438df7
fabio brasil <fabiobfelix@hotmail.com>
2022-02-21 15:47:42 -0300

    adicionando tag swagger users

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,43 +1,61 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { get } from 'http';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UpdateUserDto } from './dto/update-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('api/users')
+@ApiTags('users')
 @UseGuards(AuthGuard('jwt'))
 export class UsersController {
-   constructor(private readonly usersService: UsersService) {}
-   
-    @Get()
-    async index() {
-        return await this.usersService.findAll();
-    }
-        
-    @Post()
-    async store(@Body() body: CreateUserDto) {
-        return await this.usersService.store(body);
-    }
-    
-    @Get(':id')
-    async show(@Param('id', new ParseUUIDPipe()) id: string) {
-        return await this.usersService.findOneOrFail({ id });
-    }
-    
-    @Put(':id')
-    async update(
-        @Param('id', new ParseUUIDPipe()) id: string,
-        @Body() body: UpdateUserDto,
-    ) {
-        return await this.usersService.update(id, body);
-    }
+  constructor(private readonly usersService: UsersService) {}
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async destory(@Param('id', new ParseUUIDPipe()) id: string){
-        await this.usersService.destroy(id);
-    }
+  @Get()
+  async index() {
+    return await this.usersService.findAll();
+  }
 
+  @Post()
+  @ApiTags('users')
+  @ApiBody({ type: CreateUserDto })
+  async store(@Body() body: CreateUserDto) {
+    return await this.usersService.store(body);
+  }
 
+  @Get(':id')
+  @ApiTags('users')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.usersService.findOneOrFail({ id });
+  }
+
+  @Put(':id')
+  @ApiTags('users')
+  @ApiBody({ type: UpdateUserDto })
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  @ApiTags('users')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destory(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.usersService.destroy(id);
+  }
 }
