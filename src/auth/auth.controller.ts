4424f94f084024eb51def9d0dfc2c0987e381469
fabio brasil <fabiobfelix@hotmail.com>
2022-02-21 15:49:15 -0300

    adicionando tag swagger auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 
 @Controller('api/auth')
 export class AuthController {
-    constructor(private readonly authSevice : AuthService){
+  constructor(private readonly authSevice: AuthService) {}
 
-    }
-
-    @UseGuards(AuthGuard('local'))
-    @Post('login')
-    async login(@Req() req: any) {
-        return await this.authSevice.login(req.user);
-    }
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  @ApiTags('produtos')
+  async login(@Req() req: any) {
+    return await this.authSevice.login(req.user);
+  }
 }
