8943a1060c2a06e73005598db6c57aaa2cc492b1
fabio brasil <fabiobfelix@hotmail.com>
2022-02-21 15:40:33 -0300

    adicionando rota de paginação

--- src/app/produto/produto.controller.ts
+++ src/app/produto/produto.controller.ts
@@ -1,35 +1,71 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiQuery, ApiTags } from '@nestjs/swagger';
+
+import { Produto } from './entities/produto.entity';
+import { type } from 'os';
+import { number } from 'yargs';
+import { Pagination } from 'nestjs-typeorm-paginate';
 
 @Controller('produto')
 @UseGuards(AuthGuard('jwt'))
 export class ProdutoController {
-  constructor(private readonly produtoService: ProdutoService) {}
+  constructor(private readonly produtoService: ProdutoService) { }
 
   @Post()
-  create(@Body() createProdutoDto: CreateProdutoDto) {
-    return this.produtoService.create(createProdutoDto);
+  @ApiTags('produtos')
+  @ApiBody({ type: CreateProdutoDto })
+  async create(@Body() createProdutoDto: CreateProdutoDto) {
+    return await this.produtoService.create(createProdutoDto);
   }
 
+  // @Get()
+  // @ApiTags('produtos')
+  // findAll() {
+  //   return this.produtoService.findAll();
+  // }
+
   @Get()
-  findAll() {
-    return this.produtoService.findAll();
+  @ApiTags('produtos')
+  @ApiQuery({ name: 'page', required: false, type: number })
+  @ApiQuery({ name: 'limit', required: false, type: number })
+  async Index(
+    @Query('page') page = 1,
+    @Query('limit') limit = 100,
+  ): Promise<Pagination<Produto>> {
+    limit = limit > 100 ? 100 : limit;
+
+    return await this.produtoService.findAll({ page, limit });
   }
 
   @Get(':id')
+  @ApiTags('produtos')
   findOne(@Param('id') id: string) {
     return this.produtoService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiTags('produtos')
+  @ApiBody({ type: UpdateProdutoDto })
   update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
     return this.produtoService.update(+id, updateProdutoDto);
   }
 
   @Delete(':id')
+  @ApiTags('produtos')
   remove(@Param('id') id: string) {
     return this.produtoService.remove(+id);
   }
--- src/app/produto/produto.service.ts
+++ src/app/produto/produto.service.ts
@@ -4,38 +4,62 @@ import { UpdateProdutoDto } from './dto/update-produto.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Produto } from './entities/produto.entity';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class ProdutoService {
   constructor(
     @InjectRepository(Produto)
-    private readonly produtoRepository: Repository<Produto>
+    private readonly produtoRepository: Repository<Produto>,
+  ) {}
 
-  ) { }
-
-  create(createProdutoDto: CreateProdutoDto): Promise <Produto> {
-    const produto = this.produtoRepository.create(createProdutoDto)
-    return this.produtoRepository.save(produto)
+  create(createProdutoDto: CreateProdutoDto): Promise<Produto> {
+    const produto = this.produtoRepository.create(createProdutoDto);
+    return this.produtoRepository.save(produto);
   }
 
-  findAll(): Promise<Produto[]> {
-    return this.produtoRepository.find();
+  // findAll(): Promise<Produto[]> {
+  //   return this.produtoRepository.find();
+  // }
+
+  async findAll(options: IPaginationOptions): Promise<Pagination<Produto>> {
+    const queryBuilder = this.produtoRepository.createQueryBuilder('p');
+
+    queryBuilder.select([
+      'p.id',
+      'p.nome',
+      'p.descricao',
+      'p.preco',
+      'p.quantidade_disponivel',
+    ]);
+
+    queryBuilder.orderBy('p.id', 'ASC');
+
+    return paginate<Produto>(queryBuilder, options);
   }
 
   findOne(id: number): Promise<Produto> {
     return this.produtoRepository.findOne(id);
   }
 
-  async update(id: number, updateProdutoDto: UpdateProdutoDto): Promise <Produto> {
+  async update(
+    id: number,
+    updateProdutoDto: UpdateProdutoDto,
+  ): Promise<Produto> {
     const produto = await this.produtoRepository.preload({
       id: id,
-      ...updateProdutoDto
+      ...updateProdutoDto,
     });
-    if(!produto){
-      throw new NotFoundException(`Produto ${id} não econtrado, verifique o id digitado e tente novamente!`) 
-
+    if (!produto) {
+      throw new NotFoundException(
+        `Produto ${id} não econtrado, verifique o id digitado e tente novamente!`,
+      );
     }
-   
+
     return this.produtoRepository.save(produto);
   }
 
